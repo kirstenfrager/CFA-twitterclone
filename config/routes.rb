dc9559be97bf76cf43aa0514ead892afe265204c
Kirsten Frager <kirstenfrager@kirstens-air.ait-sydney.net.au>
2017-03-16 13:50:54 +1100

    nav routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
 
+  # get 'pages/posts'
+  # **************
+
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
