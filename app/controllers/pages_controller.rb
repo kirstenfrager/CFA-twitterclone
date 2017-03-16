184ad8b8d8000dff0a86542de55c87825895f2ac
Kirsten Frager <kirstenfrager@kirstens-air.ait-sydney.net.au>
2017-03-16 15:24:45 +1100

    user id added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
-  before_action :authenticate_user!
+  skip_before_action :authenticate_user!
 
   def home
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'kiki@kiki', password: '12345678')
+User.create(email: 'al@al', password: '12345678')
